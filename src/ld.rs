e4672c2faccc4966ef8a5c74099b943cc4b276e9
Yannick Feld <yannick.feld@uol.de>
2023-01-16 16:14:54 +0100

    refactor

--- src/ld.rs
+++ src/ld.rs
@@ -3,9 +3,11 @@ mod ld_model;
 mod exec;
 mod sir_writer;
 mod print_dot;
+mod mutation_scan;
 
 pub use param::*;
 pub use ld_model::*;
 pub use exec::*;
 pub use sir_writer::*;
 pub use print_dot::*;
+pub use mutation_scan::*;
