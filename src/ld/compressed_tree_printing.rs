e110dffeacfbd68329efd22d72bcb8d23388036d
Yannick Feld <yannick.feld@uol.de>
2023-04-27 13:59:51 +0200

    working but ugly tree gp

--- src/ld/compressed_tree_printing.rs
+++ src/ld/compressed_tree_printing.rs
@@ -16,7 +16,13 @@ pub struct CompressedTreeOptions
 
     /// Print the print_idxTH tree that fulfills the other criteria
     #[structopt(long, short)]
-    pub print_idx: Option<usize>
+    pub print_idx: Option<usize>,
+
+    #[structopt(long, short)]
+    pub output_name: String,
+
+    #[structopt(long)]
+    pub pdf: bool
 }
 
 
@@ -42,7 +48,7 @@ pub fn compressed_tree_printer(opts: CompressedTreeOptions)
             };
             if valid_c{
                 if current_idx == print_idx {
-                    create_gp(info_graph);
+                    create_gp(info_graph, &opts);
                     return;
                 }
                 current_idx += 1;
@@ -51,7 +57,7 @@ pub fn compressed_tree_printer(opts: CompressedTreeOptions)
     }
 }
 
-fn create_gp(info_graph: InfoGraph)
+fn create_gp(info_graph: InfoGraph, opts: &CompressedTreeOptions)
 {
     
 
@@ -76,11 +82,16 @@ fn create_gp(info_graph: InfoGraph)
     let initial_infected = info_graph.initial_infection[0];
 
     let mut husks = vec![None; info_graph.info.vertex_count()];
-    let mut number_of_nodes: usize = 0;
+    
+    let mut max_depth = 0;
 
     for (index, node, depth) in  info_graph.info.bfs_index_depth(initial_infected)
     {
-        number_of_nodes += 1;
+        if depth > max_depth 
+        {
+            max_depth = depth;
+        }
+        
         let species = if index < info_graph.dog_count {
             HumanOrDog::Dog
         } else {
@@ -93,7 +104,6 @@ fn create_gp(info_graph: InfoGraph)
             SegmentVerticalHusk{
                 depth,
                 species,
-                num_descendants: descendant_count[index],
                 children: Vec::new()
             }
         );
@@ -105,13 +115,58 @@ fn create_gp(info_graph: InfoGraph)
         }
     }
 
-    let mut husks_y = vec![None; husks.len()];
+    let mut husks_specifying_space_req: Vec<_> = 
+        husks.iter()
+            .enumerate()
+            .map(
+                |(index, husk)|
+                {
+                    husk.clone().map(
+                        |husk|
+                        {
+                            SegmentVerticalHuskSpaceReq{
+                                species: husk.species,
+                                depth: husk.depth,
+                                children: husk.children,
+                                space_req: 1,
+                                index
+                            }
+                        }
+                    )
+                }
+            ).collect();
     
-    
-    let initial = husks[initial_infected].as_ref().unwrap();
-    
+
+    let mut depth_map = vec![Vec::new(); max_depth + 1];
+
+    for (index, _, depth) in info_graph.info.bfs_index_depth(initial_infected)
+    {
+        depth_map[depth].push(index);
+    }
+
+    while let Some(depth_list) = depth_map.pop()
+    {
+        for node_idx in depth_list
+        {
+            let this = husks_specifying_space_req[node_idx].as_mut().unwrap();
+            this.space_req += this.children.len().saturating_sub(1);
+
+            let space_req = this.space_req;
+
+            if let InfectedBy::By(by) = info_graph.info.at(node_idx).infected_by
+            {
+                let parent = husks_specifying_space_req[by as usize].as_mut().unwrap();
+                parent.space_req += space_req;
+            }
+        }
+    }
+
+    let mut husks_y =vec![None; husks_specifying_space_req.len()];
+
+    let initial = husks_specifying_space_req[initial_infected].as_ref().unwrap();
+
     husks_y[initial_infected] = Some(
-        initial.with_y(0, number_of_nodes)
+        initial.with_y(0)
     );
 
 
@@ -121,33 +176,19 @@ fn create_gp(info_graph: InfoGraph)
     {
         let parent = husks_y[idx].as_ref().unwrap();
         let y_min = parent.y_min;
-        let y_max = parent.y_max;
 
         let mut current_y = y_min;
 
-        let total_children = parent.children.len();
-
-
         // how I need to assign all children husks.
-        for (child_count, &child) in husks[idx].as_ref().unwrap().children.iter().enumerate()
+        for &child in husks_specifying_space_req[idx].as_ref().unwrap().children.iter()
         {
-            let child_husk = husks[child as usize].as_ref().unwrap();
+            let child_husk = husks_specifying_space_req[child as usize].as_ref().unwrap();
             let child_y_min = current_y;
-            // if last child
-            let child_y_max = if child_count == total_children - 1
-            {
-                dbg!("last child");
-                current_y += 1 + child_husk.num_descendants as usize;
-                current_y
-            } else {
-                current_y += 1 + child_husk.num_descendants as usize;
-                current_y
-            };
-
-            current_y += 1;
+            
+            current_y += child_husk.space_req + 1; // eventuell noch + 1
 
             husks_y[child as usize] = Some(
-                child_husk.with_y(child_y_min, child_y_max)
+                child_husk.with_y(child_y_min)
             );
 
             stack.push(child as usize);
@@ -156,15 +197,24 @@ fn create_gp(info_graph: InfoGraph)
 
     stack.push(initial_infected);
 
-    let file = File::create("test_compressed.gp").unwrap();
+    let name = format!("{}.gp", opts.output_name);
+    println!("creating {name}");
+
+    let file = File::create(name).unwrap();
     let mut buf = BufWriter::new(file);
     let mut counter = 0;
     let mut colors = Vec::new();
 
+    if opts.pdf{
+        writeln!(buf, "set t pdf").unwrap();
+        writeln!(buf, "set output \"{}.pdf\"", opts.output_name).unwrap();
+    }
+
+
     while let Some(idx) = stack.pop()
     {
         let parent = husks_y[idx].as_ref().unwrap();
-        parent.draw(&mut buf, &mut counter, &mut colors, &husks);
+        parent.draw(&mut buf, &mut counter, &mut colors, &husks, &info_graph);
         for (_, &child) in husks[idx].as_ref().unwrap().children.iter().enumerate()
         {
             stack.push(child as usize);
@@ -176,7 +226,9 @@ fn create_gp(info_graph: InfoGraph)
         writeln!(buf, "$data{i} w l lc {} t \"\",\\", color.line_color()).unwrap();
     }
     writeln!(buf).unwrap();
-
+    if opts.pdf{
+        writeln!(buf, "set output").unwrap();
+    }
 }
 
 
@@ -192,34 +244,44 @@ struct SegmentVerticalHusk
 {
     depth: usize,
     species: HumanOrDog,
-    num_descendants: u16,
     children: Vec<u16>
 } 
 
-impl SegmentVerticalHusk{
-    pub fn with_y(&self, y_min: usize, y_max: usize) -> SegmentVerticalHuskY
+
+
+#[derive(Clone)]
+struct SegmentVerticalHuskSpaceReq
+{
+    depth: usize,
+    species: HumanOrDog,
+    children: Vec<u16>,
+    space_req: usize,
+    index: usize
+}
+
+impl SegmentVerticalHuskSpaceReq{
+    pub fn with_y(&self, y_min: usize) -> SegmentVerticalHuskY
     {
         SegmentVerticalHuskY{
             depth: self.depth,
             species: self.species,
             children: self.children.clone(),
-            num_descendants: self.num_descendants,
             y_min,
-            y_max
+            y_max: y_min + self.space_req,
+            index: self.index
         }
     }
 }
-
 
 #[derive(Clone)]
 struct SegmentVerticalHuskY
 {
     depth: usize,
     species: HumanOrDog,
-    num_descendants: u16,
     children: Vec<u16>,
     y_min: usize,
-    y_max: usize
+    y_max: usize,
+    index: usize
 }
 
 pub enum Color{
@@ -247,7 +309,8 @@ impl SegmentVerticalHuskY{
         mut writer: W, 
         eof_counter: &mut usize, 
         color_vec: &mut Vec<Color>,
-        husks: &[Option<SegmentVerticalHusk>]
+        husks: &[Option<SegmentVerticalHusk>],
+        info: &InfoGraph
     )
     {
         if self.children.len() > 1 {
@@ -270,16 +333,18 @@ impl SegmentVerticalHuskY{
         let mut draw_edge = |y_pos, color| {
             writeln!(writer, "$data{eof_counter}<<EOF").unwrap();
             writeln!(writer, "{} {}", self.depth, y_pos).unwrap();
-            writeln!(writer, "{} {}", self.depth + 1, y_pos).unwrap();
+            writeln!(writer, "{} {}", self.depth - 1, y_pos).unwrap();
             writeln!(writer, "EOF").unwrap();
             *eof_counter += 1;
             color_vec.push(color);
         };
 
-        if self.children.len() == 1 {
+        let index = self.index;
+        if let InfectedBy::By(by) = info.info.at(index).infected_by
+        {
+            let parent_species = husks[by as usize].as_ref().unwrap().species;
             let mid = self.y_min as f64 + ((self.y_max  - self.y_min) as f64).abs() / 2.0;
-            let child_species = husks[self.children[0] as usize].as_ref().unwrap().species;
-            let color = match (child_species, self.species)
+            let color = match (self.species, parent_species)
             {
                 (HumanOrDog::Human, HumanOrDog::Human) => Color::Blue,
                 (HumanOrDog::Dog, HumanOrDog::Dog) => Color::Black,
@@ -287,8 +352,6 @@ impl SegmentVerticalHuskY{
                 (HumanOrDog::Human, HumanOrDog::Dog) => Color::Magenta
             };
             draw_edge(mid, color);
-        } else {
-            
         }
 
         
